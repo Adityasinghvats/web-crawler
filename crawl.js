7e23df328d689af2347e8bfe9dded7c366421c7f
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2025-01-25 01:31:15 +0530

    add getURLsfromHTML function and corresponding tests; update package.json to include jsdom dependency

--- crawl.js
+++ crawl.js
@@ -1,3 +1,33 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsfromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody) //creates a in memory copy of DOM
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for(const linkElement of linkElements){
+        // console.log(linkElement.href)
+        if(linkElement.href.slice(0, 1) === '/'){
+            //relative
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            }catch(err){
+                console.log(`Error ${err.message}`)
+            }
+            
+        }else{
+            //absolute
+            try{
+                const urlObj = new URL(linkElement)
+                urls.push(urlObj.href)
+            }catch(err){
+                console.log(`Error ${err.message}`)
+            }
+        }
+    }
+    return urls
+}
+
 function normalizeURL(urlString){
     const urlObj = new URL(urlString)   
     const hostpath =  `${urlObj.hostname}${urlObj.pathname}`
@@ -8,5 +38,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsfromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,30 +1,97 @@
-const {normalizeURL} = require ('./crawl')
+const {normalizeURL, getURLsfromHTML} = require ('./crawl')
 const {test , expect} = require('@jest/globals')
 
 test('normalizeURL strip protocal',() => {
-    const input = "https://blog.boot.dev/path"
+    const input = "https://github.com/Adityasinghvats/path/"
     const actual = normalizeURL(input)
-    const expected = "blog.boot.dev/path"
+    const expected = "github.com/Adityasinghvats/path"
     expect(actual).toEqual(expected)
 })
 
 test('normalizeURL trailing slash',() => {
-    const input = "https://blog.boot.dev/path/"
+    const input = "https://github.com/Adityasinghvats/path/"
     const actual = normalizeURL(input)
-    const expected = "blog.boot.dev/path"
+    const expected = "github.com/Adityasinghvats/path"
     expect(actual).toEqual(expected)
 })
 
 test('normalizeURL capitals',() => {
-    const input = "https://Blog.boot.dev/path/"
+    const input = "https://Github.com/Adityasinghvats/path/"
     const actual = normalizeURL(input)
-    const expected = "blog.boot.dev/path"
+    const expected = "github.com/Adityasinghvats/path"
     expect(actual).toEqual(expected)
 })
 
 test('normalizeURL protocol',() => {
-    const input = "http://Blog.boot.dev/path/"
+    const input = "http://github.com/Adityasinghvats/path/"
     const actual = normalizeURL(input)
-    const expected = "blog.boot.dev/path"
+    const expected =  "github.com/Adityasinghvats/path"
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML absolute urls',() => {
+    const inputHTML = `
+    <html>
+      <body>
+        <a href="https://github.com/Adityasinghvats/path/">
+        Github
+        </a>
+      </body>
+    </html>  
+    `
+    const baseurl = "https://Github.com/Adityasinghvats/path/"
+    const actual = getURLsfromHTML(inputHTML, baseurl)
+    const expected = ["https://github.com/Adityasinghvats/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML relative urls',() => {
+    const inputHTML = `
+    <html>
+      <body>
+        <a href="/path/">
+        Github
+        </a>
+      </body>
+    </html>  
+    `
+    const baseurl = "https://github.com/Adityasinghvats"
+    const actual = getURLsfromHTML(inputHTML, baseurl)
+    const expected = ["https://github.com/Adityasinghvats/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML mixed urls',() => {
+    const inputHTML = `
+    <html>
+      <body>
+        <a href="https://github.com/Adityasinghvats/path1/">
+        Github
+        </a>
+        <a href="/path2/">
+        Github
+        </a>
+      </body>
+    </html>  
+    `
+    const baseurl = "https://github.com/Adityasinghvats"
+    const actual = getURLsfromHTML(inputHTML, baseurl)
+    const expected = ["https://github.com/Adityasinghvats/path1/","https://github.com/Adityasinghvats/path2/"]
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsfromHTML invalid urls',() => {
+    const inputHTML = `
+    <html>
+      <body>
+        <a href="invalid">
+         Invalid Url
+        </a>
+      </body>
+    </html>  
+    `
+    const baseurl = "https://github.com/Adityasinghvats"
+    const actual = getURLsfromHTML(inputHTML, baseurl)
+    const expected = []
     expect(actual).toEqual(expected)
 })
